eaf90c2a9aced904fca677fa02ec9beae9ccbd02
trukkerweeler <ken22008@byui.edu>
2023-11-14 21:59:26 -0700

    timo_1114

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import theme from '../../tailwind.config.js'
+// import postcss from 'postcss.config.js'
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+      <div className={styles.section}>
         Start1
       </div>
 
