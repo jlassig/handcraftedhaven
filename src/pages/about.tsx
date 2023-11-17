77e1a7f69a00df58b4cd9a6584618b9ab12032e2
jlassig <las22004@byui.edu>
2023-11-16 22:30:32 -0700

    added our folder/file structure

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const About: React.FC = () => {
+  return (
+    <Layout>
+      <h1>About Us</h1>
+      <p>About page goes here.</p>
+    </Layout>
+  );
+};
+
+export default About;
