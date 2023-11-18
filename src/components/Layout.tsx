ba09a2cce747198c11e3538dd84c81f356695563
jlassig <las22004@byui.edu>
2023-11-17 22:03:51 -0700

    weird issues from last night

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 
 //import type { Metadata } from 'next'
 //import { Inter } from 'next/font/google'
-import '../styles/globals.css'
+
 import React, { ReactNode } from 'react';
 import Header from './Header'
 import Footer from './Footer'
