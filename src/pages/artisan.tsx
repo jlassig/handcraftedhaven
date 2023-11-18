0e9561d74077c0e7e2d8f6c5f55817c5917ee81e
jlassig <las22004@byui.edu>
2023-11-18 00:18:05 -0700

    added pictures

--- src/pages/artisan.tsx
+++ src/pages/artisan.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Hero from '../components/Hero';
 
-const About: React.FC = () => {
+const Artisan: React.FC = () => {
   return (
     <Layout>
-      <Hero title="About Us"/>
-      <p>About page goes here.</p>
+      <Hero title="Our Artisans"/>
+      <p>Artisan profiles go here.</p>
     </Layout>
   );
 };
 
-export default About;
+export default Artisan;
