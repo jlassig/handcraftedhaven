8dc0983fc53aa89b65875db65c639213e13fbe08
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-11-18 19:20:46 -0600

    ci: Add more complete footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,34 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer>
-      <p>&copy; 2023 Handcrafted Haven</p>
+    <footer className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between">
+        {/* Column 1 */}
+        <div className="flex flex-col">
+          <h2 className="text-lg font-bold mb-2">Shop</h2>
+          <a href="#">Artisans</a>
+          <a href="#">Products</a>
+          <a href="#">Categories</a>
+        </div>
+
+        {/* Column 2 */}
+        <div className="flex flex-col">
+          <h2 className="text-lg font-bold mb-2">Information</h2>
+          <a href="#">About Us</a>
+          <a href="#">Contact</a>
+          <a href="#">FAQs</a>
+        </div>
+
+        {/* Column 3 */}
+        <div className="flex flex-col">
+          <h2 className="text-lg font-bold mb-2">Connect With Us</h2>
+          <a href="#">Facebook</a>
+          <a href="#">Instagram</a>
+          <a href="#">Twitter</a>
+        </div>
+      </div>
+
+      <p className="text-center mt-4">&copy; 2023 Handcrafted Haven. All rights reserved.</p>
     </footer>
   );
 };
